4fce62896b4090f1f6e79a50f4f1fc59b769380c
DebbieElabonga <debbie.elabonga@student.moringaschool.com>
2021-06-18 22:29:13 +0300

    navbar.html

--- app/request.py
+++ app/request.py
@@ -1,7 +1,7 @@
-from app.models.quote_test import Quote
-from app import app
+
 import urllib.request,json
 from .models import quote
+from app import app
 
 Quote = quote.Quote
 
@@ -21,29 +21,6 @@ def get_quotes():
         quotes_results = None
 
         if get_quotes_response:
-            quotes_results_list = get_quotes_response['results']
-            quotes_results = process_results(quotes_results_list)
-
-
-    return quotes_results
-def process_results(quote_list):
-    '''
-    Function  that processes the quote result and transform them to a list of Objects
-
-    Args:
-        quote_list: A list of dictionaries that contain quote details
-
-    Returns :
-        quote_results: A list of movie objects
-    '''
-    quote_results = []
-    for quote_item in quote_list:
-        id = quote_item.get('id')
-        author = quote_item.get('author')
-        quote = quote_item.get('quote')
-        
-        quote_object = quote(id,author,quote)
-        quote_results.append(quote_object)
-
-    return quote_results
-    
+            quotes_results_list = get_quotes_response
+            
+    return quotes_results_list
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_quotes
 
 # Views
 @app.route('/')
@@ -8,4 +9,6 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    #Getting quotes
+    quotes = get_quotes()
+    return render_template('index.html',quotes=quotes)
