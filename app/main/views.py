58d6e178b558c5ff5293971f74cbe4705f410ea6
DebbieElabonga <debbie.elabonga@student.moringaschool.com>
2021-06-19 21:28:59 +0300

    profile pic

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,10 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..request import get_quotes
-from flask_login import login_required
+from ..models import Blog, User,Comment
+from .forms import UpdateProfile
+from .. import db,photos
+from flask_login import login_user,logout_user,login_required
 # Views
 @main.route('/')
 def index():
@@ -12,3 +15,42 @@ def index():
     #Getting quotes
     quotes = get_quotes()
     return render_template('index.html',quotes=quotes)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     QUOTES_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://hamisi:1995@localhost/blog'
-    
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 
 class ProdConfig(Config):
